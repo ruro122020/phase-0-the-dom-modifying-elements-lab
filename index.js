dd8b174a7d996120febc5411c26ecb1dd7aa2018
RuthRojas <ruro122020@gmail.com>
2023-07-08 12:38:26 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//test 1
+main.remove();
+//test 2
+const newHeader = document.createElement('h1');
+//test 3
+newHeader.setAttribute('id', 'victory');
+//test 4
+newHeader.textContent = 'Chelsea is the champion';
